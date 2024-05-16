3a18c3564f5640d9e4bef9734b114bc289b9bb50
vishaal-d <vishaal_d@outlook.com>
2024-05-17 01:33:59 +0530

    Hero Page

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -10,7 +10,7 @@ const MaxWidthWrapper = ({className, children}: {
             className={cn(
                 'h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
             className
-        )}></div>)
+        )}>{children}</div>)
 }
 
 export default MaxWidthWrapper
